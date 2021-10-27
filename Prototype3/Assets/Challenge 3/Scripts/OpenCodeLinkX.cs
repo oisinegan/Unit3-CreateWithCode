b28d5b9d85dc6db71c28ade9d7117b72f4cb6e01
oisinegan <oisinegan00@gmail.com>
2021-10-27 16:14:53 +0100

    Add Code Repo button
    Added code repository button to link github repository to game

---
+++ Prototype3/Assets/Challenge 3/Scripts/OpenCodeLinkX.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenCodeLinkX : MonoBehaviour
+{
+    public void openCodeRepo()
+    {
+        Application.OpenURL("https://github.com/oisinegan/Unit3-CreateWithCode");
+    }
+}
